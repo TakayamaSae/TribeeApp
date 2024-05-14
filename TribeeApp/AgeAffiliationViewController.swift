06b70b66b95e3d809d9d7299ad4d069944c87e64
saetakayama <dornonthexmaseve@au.com>
2024-05-14 21:40:19 +0900

    レイアウト作成１

---
+++ TribeeApp/AgeAffiliationViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AgeAffiliationViewController.swift
+//  TribeeApp
+//
+//  Created by 高山彩愛 on 2024/05/14.
+//
+
+import UIKit
+
+/// 年齢所属画面
+final class AgeAffiliationViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - IBOutlets
+    
+    
+    // MARK: - View Life-Cycle Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - IBActions
+    
+    
+    // MARK: - Other Methods
+
+}
